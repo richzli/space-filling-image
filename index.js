1aa83c943254fef071c1249b6299ecb84db249c7
Richard Li <richzli01@gmail.com>
2020-12-02 16:24:19 -0600

    css'ing

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+document.getElementById("browse").onclick = () => { document.getElementById('img-input').click(); };
+document.getElementById("img-input").onchange = () => {
+    var image = document.getElementById("img-input").files[0];
+    document.getElementById("selected-file").innerText = image.name;
+};
+
 function doIt() {
-    var image = document.querySelector("#img-input").files[0];
+    var image = document.getElementById("img-input").files[0];
     alert("ya!");
 }
