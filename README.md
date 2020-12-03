3e6d7236681faa478d76a7a96e976c83891bca54
Richard Li <richzli01@gmail.com>
2020-12-03 00:36:24 -0600

    z-order done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # space-filling-image
 
-This is my final project for Purdue's MA 440 (Honors Real Analysis) class. Inspired by discussions of cardinality, this script takes a 
+This is my final project for Purdue's MA 440 (Honors Real Analysis) class. Inspired by discussions of cardinality, this script takes a image, resizes it to 32x32, then follows the path of a few space-filling curves along the pixels of the image.
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.getElementById("unroll").disabled = true;
 document.getElementById("browse").onclick = () => { document.getElementById('img-input').click(); };
 document.getElementById("img-input").onchange = () => {
     var input = document.getElementById("img-input").files[0];
@@ -8,10 +9,67 @@ document.getElementById("img-input").onchange = () => {
     img.onload = () => {
         var cvs = document.getElementById("original");
         var ctx = cvs.getContext("2d");
-        ctx.drawImage(img, 0, 0, 32, 32);
+        ctx.clearRect(0, 0, cvs.width, cvs.height);
+        ctx.drawImage(img, 0, 0, cvs.width, cvs.height);
     };
+
+    document.getElementById("unroll").disabled = false;
 };
 
-function doIt() {
-    alert("ya!");
+function do_it() {
+    var cvs = document.getElementById("original");
+    var ctx = cvs.getContext("2d");
+
+    var z_cvs = document.getElementById("z-order");
+    var z_ctx = z_cvs.getContext("2d");
+    z_ctx.clearRect(0, 0, z_cvs.width, z_cvs.height);
+
+    var h_cvs = document.getElementById("z-order");
+    var h_ctx = z_cvs.getContext("2d");
+    h_ctx.clearRect(0, 0, h_cvs.width, h_cvs.height);
+
+    var p_cvs = document.getElementById("z-order");
+    var p_ctx = z_cvs.getContext("2d");
+    p_ctx.clearRect(0, 0, p_cvs.width, p_cvs.height);
+
+    for (var i = 0; i < 1024; ++i) {
+        var [x, y] = z_order_coords(i);
+
+        var color = ctx.getImageData(x, y, 1, 1).data;
+
+        draw_stripe(z_ctx, i, color);
+    }
+}
+
+function draw_stripe(ctx, x, color) {
+    ctx.lineWidth = "1";
+    ctx.strokeStyle = "#" + color[0].toString(16) + color[1].toString(16) + color[2].toString(16) + color[3].toString(16);
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, 49);
+    ctx.stroke();
+}
+
+function z_order_coords(index) {
+    var x_digits = index & 0b0101010101;
+    var y_digits = index & 0b1010101010;
+    
+    var reduce = num => {
+        var r = 0;
+        for (i = 0; i < 5; ++i) {
+            r += (1 << i) * (num & 1);
+            num >>= 2;
+        }
+        return r;
+    }
+
+    return [reduce(x_digits), reduce(y_digits >> 1)];
+}
+
+function hilbert_coords(index) {
+    return [0, 0];
+}
+
+function peano_coords(index) {
+    return [0, 0];
 }
