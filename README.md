80d85dad1c76774f8e4739efac7fe5ed3af3dccf
Richard Li <richzli01@gmail.com>
2020-12-02 18:53:37 -0600

    image upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# space-filling-image
+
+This is my final project for Purdue's MA 440 (Honors Real Analysis) class. Inspired by discussions of cardinality, this script takes a 
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 document.getElementById("browse").onclick = () => { document.getElementById('img-input').click(); };
 document.getElementById("img-input").onchange = () => {
-    var image = document.getElementById("img-input").files[0];
-    document.getElementById("selected-file").innerText = image.name;
+    var input = document.getElementById("img-input").files[0];
+    document.getElementById("selected-file").innerText = input.name;
+
+    var img = new Image();
+    img.src = URL.createObjectURL(input);
+    img.onload = () => {
+        var cvs = document.getElementById("original");
+        var ctx = cvs.getContext("2d");
+        ctx.drawImage(img, 0, 0, 32, 32);
+    };
 };
 
 function doIt() {
-    var image = document.getElementById("img-input").files[0];
     alert("ya!");
 }
